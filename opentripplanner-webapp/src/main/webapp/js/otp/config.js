cac618133507ff2a6618f7c75ceb6a9c6dc0d06c
Vivien Deparday <vivien.deparday@gmail.com>
2012-01-13 10:08:05 -0500

    Enable and configure the geocoder

--- opentripplanner-webapp/src/main/webapp/js/otp/config.js
+++ opentripplanner-webapp/src/main/webapp/js/otp/config.js
@@ -57,11 +57,11 @@ otp.config_defaults = {
 
         geocoder  :
         {
-            enabled : false,
-            url     : "/geocoder/geocode",  
+            enabled : true,
+            url     : "/opentripplanner-geocoder/geocode",
             addressParamName : "address"
-        },
-        fromToOverride : new Ext.Template('<div class="mapHelp">' + otp.config.locale.config.rightClickMsg + '</div>')
+        }//,
+        //fromToOverride : new Ext.Template('<div class="mapHelp">' + otp.config.locale.config.rightClickMsg + //'</div>')
 
         /* debug geocoder */
         /* *
